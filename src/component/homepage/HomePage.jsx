6811e27d911051b3d20ff166712ef43ca893ce01
rouie <streetstar03@gmail.com>
2024-10-16 17:21:10 +0900

    피그마 코드 추출

---
+++ src/component/homepage/HomePage.jsx
@@ -0,0 +1,1149 @@
+/* 2. 게시판 메인 페이지 */
+
+position: relative;
+width: 430px;
+height: 932px;
+
+background: #FFF4D2;
+
+
+/* Rectangle 47 */
+
+position: absolute;
+width: 430px;
+height: 187px;
+left: 0px;
+top: 0px;
+
+background: #FFFFFF;
+
+
+/* OO시 OO동 */
+
+position: absolute;
+width: 130px;
+height: 42px;
+left: 24px;
+top: 135px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 24.32px;
+line-height: 29px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+border: 0.5px solid #000000;
+
+
+/* Group 42 */
+
+position: absolute;
+width: 406px;
+height: 248px;
+left: 10px;
+top: 555px;
+
+
+
+/* Group 13 */
+
+position: absolute;
+width: 406px;
+height: 248px;
+left: 10px;
+top: 555px;
+
+
+
+/* Rectangle 46 */
+
+position: absolute;
+width: 406px;
+height: 205px;
+left: calc(50% - 406px/2 - 2px);
+top: 598px;
+
+background: #FFFBFB;
+box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.25);
+border-radius: 30px;
+
+
+/* 자유 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 607px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 자유 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 638px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 지역 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 669px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 지역 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 700px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 번개 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 731px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 번개 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 762px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 최근 게시물 */
+
+position: absolute;
+width: 135px;
+height: 43px;
+left: 28px;
+top: 555px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 24.32px;
+line-height: 29px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+border: 0.5px solid #000000;
+
+
+/* Vector */
+
+position: absolute;
+left: 37.44%;
+right: 55.58%;
+top: 61.27%;
+bottom: 37.6%;
+
+background: #ED6000;
+
+
+/* Vector */
+
+position: absolute;
+width: 22px;
+height: 22px;
+left: 271px;
+top: 145px;
+
+background: #000000;
+
+
+/* Notification */
+
+position: absolute;
+width: 30px;
+height: 30px;
+left: 318px;
+top: 141px;
+
+
+
+/* Vector */
+
+position: absolute;
+left: 16.67%;
+right: 16.67%;
+top: 8.33%;
+bottom: 8.33%;
+
+background: #000000;
+
+
+/* Group 24 */
+
+position: absolute;
+width: 30px;
+height: 30px;
+left: 373px;
+top: 141px;
+
+
+
+/* Group 15 */
+
+position: absolute;
+width: 30px;
+height: 30px;
+left: 373px;
+top: 141px;
+
+
+
+/* Ellipse 94 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 30px;
+height: 30px;
+left: 373px;
+top: 141px;
+
+background: #F4B183;
+border: 0.5px solid #DFA67B;
+
+
+/* Vector */
+
+position: absolute;
+width: 11.25px;
+height: 11.25px;
+left: 382.38px;
+top: 150.38px;
+
+background: #ED6000;
+
+
+/* Group 39 */
+
+position: absolute;
+width: 55px;
+height: 55px;
+left: 328px;
+top: 741px;
+
+filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.25));
+
+
+/* Group 38 */
+
+position: absolute;
+width: 55px;
+height: 55px;
+left: 328px;
+top: 741px;
+
+
+
+/* Group 37 */
+
+position: absolute;
+width: 55px;
+height: 55px;
+left: 328px;
+top: 741px;
+
+
+
+/* Small FAB
+
+Use Floating action buttons (FABs) for the most important action (primary action) on a screen. The FAB appears in front of all other content on screen, and is recognizable for its rounded shape and icon in the center.
+
+A small FAB is used for a secondary, supporting action, or in place of a default FAB on smaller screens.
+
+Enabled primary FAB 
+*/
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 0px;
+
+position: absolute;
+width: 55px;
+height: 55px;
+left: 328px;
+top: 741px;
+
+background: #F4B183;
+box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3);
+border-radius: 12px;
+
+
+/* state-layer */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 8px;
+
+width: 40px;
+height: 40px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* icon
+
+Keywords: photo
+*/
+
+display: none;
+width: 24px;
+height: 24px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Group 38 */
+
+position: absolute;
+width: 35.5px;
+height: 27.58px;
+left: 338.31px;
+top: 754.75px;
+
+
+
+/* Group 37 */
+
+position: absolute;
+width: 35.5px;
+height: 27.58px;
+left: 338.31px;
+top: 754.75px;
+
+
+
+/* Group */
+
+position: absolute;
+left: 78.68%;
+right: 13.07%;
+top: 83.94%;
+bottom: 13.1%;
+
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+left: 78.68%;
+right: 13.07%;
+top: 83.94%;
+bottom: 13.1%;
+
+background: #ED6000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+left: 81.61%;
+right: 16%;
+top: 83.21%;
+bottom: 15.69%;
+
+background: #ED6000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Group 14 */
+
+position: absolute;
+width: 406px;
+height: 216px;
+left: 10px;
+top: 309px;
+
+
+
+/* Rectangle 48 */
+
+position: absolute;
+width: 406px;
+height: 173px;
+left: calc(50% - 406px/2 - 2px);
+top: 352px;
+
+background: #FFFBFB;
+box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.25);
+border-radius: 30px;
+
+
+/* 자유 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 361px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 자유 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 392px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 지역 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 423px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 지역 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 454px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 번개 게시판 [ 글 제목 ] */
+
+position: absolute;
+width: 361px;
+height: 31px;
+left: 32px;
+top: 485px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+
+
+/* 오늘의 HOT 게시물 */
+
+position: absolute;
+width: 227px;
+height: 43px;
+left: 28px;
+top: 309px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 24.32px;
+line-height: 29px;
+display: flex;
+align-items: center;
+
+color: #000000;
+
+border: 0.5px solid #000000;
+
+
+/* Group */
+
+position: absolute;
+left: 56.05%;
+right: 38.14%;
+top: 34.01%;
+bottom: 63.3%;
+
+
+
+/* Group */
+
+position: absolute;
+left: 56.05%;
+right: 38.14%;
+top: 34.01%;
+bottom: 63.3%;
+
+
+
+/* Group */
+
+position: absolute;
+left: 57.86%;
+right: 39.96%;
+top: 35.64%;
+bottom: 63.3%;
+
+
+
+/* Vector */
+
+position: absolute;
+left: 57.86%;
+right: 39.96%;
+top: 35.64%;
+bottom: 63.3%;
+
+background: #ED6000;
+
+
+/* Group */
+
+position: absolute;
+left: 56.05%;
+right: 38.14%;
+top: 34.01%;
+bottom: 63.33%;
+
+
+
+/* Vector */
+
+position: absolute;
+left: 56.05%;
+right: 38.14%;
+top: 34.01%;
+bottom: 63.33%;
+
+background: #ED6000;
+
+
+/* Group 47 */
+
+position: absolute;
+width: 430px;
+height: 100px;
+left: 0px;
+top: 832px;
+
+
+
+/* Rectangle 45 */
+
+position: absolute;
+width: 430px;
+height: 99px;
+left: 0px;
+top: 833px;
+
+background: #FFFFFF;
+
+
+/* Vector */
+
+position: absolute;
+width: 30px;
+height: 30px;
+left: calc(50% - 30px/2);
+top: 861px;
+
+background: #000000;
+
+
+/* Group */
+
+position: absolute;
+width: 30px;
+height: 30px;
+left: 123px;
+top: 860.88px;
+
+
+
+/* Group */
+
+position: absolute;
+left: 28.6%;
+right: 64.42%;
+top: 92.37%;
+bottom: 4.41%;
+
+
+
+/* Vector */
+
+position: absolute;
+left: 29.77%;
+right: 67.91%;
+top: 94.51%;
+bottom: 5.13%;
+
+background: #000000;
+
+
+/* Vector */
+
+position: absolute;
+left: 32.09%;
+right: 65.58%;
+top: 93.08%;
+bottom: 6.56%;
+
+background: #000000;
+
+
+/* Vector */
+
+position: absolute;
+left: 30.93%;
+right: 66.74%;
+top: 93.8%;
+bottom: 5.84%;
+
+background: #000000;
+
+
+/* Vector */
+
+position: absolute;
+left: 28.6%;
+right: 64.42%;
+top: 92.37%;
+bottom: 4.41%;
+
+background: #000000;
+
+
+/* Vector */
+
+position: absolute;
+width: 30px;
+height: 33.75px;
+left: 46px;
+top: 859px;
+
+background: #000000;
+
+
+/* Vector */
+
+position: absolute;
+width: 33.33px;
+height: 30px;
+left: 273px;
+top: 860.88px;
+
+background: #000000;
+
+
+/* Vector */
+
+position: absolute;
+width: 30px;
+height: 30px;
+left: 351.33px;
+top: 860.88px;
+
+background: #000000;
+
+
+/* Horizontal/Full-width
+
+Dividers are one way to visually group components and create hierarchy. They can also be used to imply nested parent/child relationships.
+
+Use full width dividers to separate larger sections of unrelated content. They can be used directly on a surface or inside other components like cards or lists.
+*/
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+padding: 0px;
+
+position: absolute;
+width: 430px;
+height: 1px;
+left: 0px;
+top: 832px;
+
+
+
+/* Divider */
+
+width: 430px;
+height: 0px;
+
+/* M3/sys/light/outline-variant */
+border: 1px solid #CAC4D0;
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+
+
+/* Frame 18 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 0px;
+gap: 20px;
+
+position: absolute;
+width: 380px;
+height: 60px;
+left: calc(50% - 380px/2);
+top: 222px;
+
+
+
+/* Group 35 */
+
+width: 60px;
+height: 60px;
+
+filter: drop-shadow(1px 1px 4px rgba(0, 0, 0, 0.25));
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Group 15 */
+
+position: absolute;
+width: 60px;
+height: 60px;
+left: 0px;
+top: 0px;
+
+
+
+/* Ellipse 94 */
+
+position: absolute;
+width: 60px;
+height: 60px;
+left: 0px;
+top: 0px;
+
+background: #F4B183;
+
+
+/* Group */
+
+position: absolute;
+left: 5%;
+right: 89.32%;
+top: 80%;
+bottom: -38.33%;
+
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+left: 5%;
+right: 92.53%;
+top: 79.99%;
+bottom: -38.32%;
+
+background: #ED6000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+left: 8.75%;
+right: 89.32%;
+top: 80%;
+bottom: -37.65%;
+
+background: #ED6000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Group 31 */
+
+width: 60px;
+height: 60px;
+
+filter: drop-shadow(1px 1px 4px rgba(0, 0, 0, 0.25));
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* Ellipse 98 */
+
+position: absolute;
+width: 60px;
+height: 60px;
+left: 80px;
+top: 0px;
+
+background: #F4B183;
+
+
+/* Vector */
+
+position: absolute;
+left: 25%;
+right: 67.11%;
+top: 21.67%;
+bottom: 19.75%;
+
+background: #ED6000;
+
+
+/* Group 33 */
+
+width: 60px;
+height: 60px;
+
+filter: drop-shadow(1px 1px 4px rgba(0, 0, 0, 0.25));
+
+/* Inside auto layout */
+flex: none;
+order: 2;
+flex-grow: 0;
+
+
+/* Group 30 */
+
+position: absolute;
+width: 60px;
+height: 60px;
+left: 160px;
+top: 0px;
+
+
+
+/* Ellipse 98 */
+
+position: absolute;
+width: 60px;
+height: 60px;
+left: 160px;
+top: 0px;
+
+background: #F4B183;
+
+
+/* Group */
+
+position: absolute;
+left: 45.53%;
+right: 45.26%;
+top: 70.95%;
+bottom: -13.57%;
+
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+left: 45.53%;
+right: 45.26%;
+top: 70.95%;
+bottom: -13.57%;
+
+background: #ED6000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Group 34 */
+
+width: 60px;
+height: 60px;
+
+filter: drop-shadow(1px 1px 4px rgba(0, 0, 0, 0.25));
+
+/* Inside auto layout */
+flex: none;
+order: 3;
+flex-grow: 0;
+
+
+/* Group 19 */
+
+position: absolute;
+width: 60px;
+height: 60px;
+left: 240px;
+top: 0px;
+
+
+
+/* Ellipse 98 */
+
+position: absolute;
+width: 60px;
+height: 60px;
+left: 240px;
+top: 0px;
+
+background: #F4B183;
+
+
+/* Group */
+
+position: absolute;
+left: 66.58%;
+right: 24.21%;
+top: 74.38%;
+bottom: -25.42%;
+
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+left: 67.96%;
+right: 25.59%;
+top: 48.85%;
+bottom: 25.63%;
+
+background: #ED6000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+left: 67.96%;
+right: 25.59%;
+top: 74.38%;
+bottom: 1.37%;
+
+background: #ED6000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+left: 66.58%;
+right: 32.49%;
+top: 69.12%;
+bottom: 15.69%;
+
+background: #ED6000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+left: 74.86%;
+right: 24.21%;
+top: 69.12%;
+bottom: 15.69%;
+
+background: #ED6000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Group 100 */
+
+width: 60px;
+height: 60px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 4;
+flex-grow: 0;
+
+
+/* Group 15 */
+
+position: absolute;
+width: 60px;
+height: 60px;
+left: 320px;
+top: 0px;
+
+filter: drop-shadow(1px 1px 4px rgba(0, 0, 0, 0.25));
+
+
+/* Ellipse 94 */
+
+position: absolute;
+width: 60px;
+height: 60px;
+left: 320px;
+top: 0px;
+
+background: #F4B183;
+
+
+/* Vector */
+
+position: absolute;
+width: 23.33px;
+height: 30px;
+left: 338px;
+top: 15px;
+
+background: #ED6000;
+
+
+/* KakaoTalk_20241010_133153218 1 */
+
+position: absolute;
+width: 85px;
+height: 85px;
+left: calc(50% - 85px/2 + 0.5px);
+top: 58px;
+
+background: url(KakaoTalk_20241010_133153218);
+border-radius: 50px;
