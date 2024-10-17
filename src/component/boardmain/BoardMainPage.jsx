a5c81113cf9aa851ee89afd54559f19f18236496
rouie <streetstar03@gmail.com>
2024-10-17 13:30:34 +0900

    작업 중

---
+++ src/component/boardmain/BoardMainPage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+function BoardMainPage() {
+    return (
+        <Main>
+
+        </Main>
+    )
+}
+
+export default BoardMainPage;
+
+const Main = styled.main`
+  background-color: #fff4d2;
+  display: flex;
+  max-width: 480px;
+  max-height: 932px;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  overflow: hidden;
+  align-items: flex-start;
+  font-family: Inter, sans-serif;
+  font-weight: 400;
+  margin: 0 auto;
+  padding: 62px 28px 313px;
+`;
