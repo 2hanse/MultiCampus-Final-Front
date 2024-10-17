2a6cde94ee1d9f3580d8de244691d27d841e073f
rouie <streetstar03@gmail.com>
2024-10-17 17:38:53 +0900

    작업 중

--- src/component/boardmain/BoardMainPage.jsx
+++ src/component/boardmain/BoardMainPage.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "./Header";
 
 function BoardMainPage() {
     return (
         <Main>
-
+            <Box />
         </Main>
     )
 }
 
-export default BoardMainPage;
-
 const Main = styled.main`
-  background-color: #fff4d2;
-  display: flex;
-  max-width: 480px;
-  max-height: 932px;
-  width: 100%;
-  height: 100%;
-  flex-direction: column;
-  overflow: hidden;
-  align-items: flex-start;
-  font-family: Inter, sans-serif;
-  font-weight: 400;
-  margin: 0 auto;
-  padding: 62px 28px 313px;
-`;
+    max-width: 430px;
+    max-height: 932px;
+    width: 100%;
+    height: 100%;
+    background: #FFF4D2;
+    display: flex;
+    overflow: hidden;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 0 auto;
+    padding: 0px 0px 932px;
+`
+
+const Box = styled.header`
+    position: relative;
+    max-width: 430px;
+    width: 100%;
+    height: 187px;
+    left: 0px;
+    top: 0px;
+    background-color: #FFFFFF;    
+`
+
+export default BoardMainPage;
---
+++ src/component/boardmain/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+    <Wrapper>
+       <Box>
+
+        </Box> 
+    </Wrapper>
+    
+}
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+const Box = styled.header`
+    position: absolute;
+    width: 430px;
+    height: 187px;
+    left: 0px;
+    top: 0px;
+
+    background-color: #FFFFFF;
+    border: 1px solid #000;
+`
+
+export default Header;
