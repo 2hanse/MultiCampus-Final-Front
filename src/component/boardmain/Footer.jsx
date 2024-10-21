6fc41c2b04b583794189755f375c0bf83d40bc6e
rouie <streetstar03@gmail.com>
2024-10-21 18:07:57 +0900

    하단 NavBar

--- src/component/boardmain/Footer.jsx
+++ src/component/boardmain/Footer.jsx
@@ -9,7 +9,11 @@ import Person from "./assets/Person.png";
 const Footer = () => {
     return (
         <FooterBox>
-            <BoardMain src={Home} alt="Home" />
+            <BoardMainPage src={Home} alt="Home" />
+            <FeedPage src={Feed} alt="Feed" />
+            <MapPage src={Map} alt="Map" />
+            <ChatPage src={Chat} alt="Chat" />
+            <MyPage Src={Person} alt="Person" />
         </FooterBox>
     );
 }
@@ -27,12 +31,43 @@ const FooterBox = styled.footer`
     z-index: 2;
 `
 
-const BoardMain = styled.img`
+const BoardMainPage = styled.img`
     position: absolute;
     width: 30px;
     height: 33.75px;
     left: 46px;
-    top: 859px;
+    top: 33%;
+`
+
+const FeedPage = styled.img`
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    left: 123px;
+    top: 35%;
+`
+const MapPage = styled.img`
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    left: calc(50% - 30px/2);
+    top: 35%;
+`
+
+const ChatPage = styled.img`
+    position: absolute;
+    width: 33.33px;
+    height: 30px;
+    left: 273px;
+    top: 35%;
+`
+
+const MyPage = styled.img`
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    left: 351.33px;
+    top: 35%;
 `
 
 export default Footer;
