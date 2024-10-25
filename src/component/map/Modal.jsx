6094b717570bc8864f104f8f79198b77bbdd1c3c
rouie <streetstar03@gmail.com>
2024-10-25 13:41:41 +0900

    Modal

---
+++ src/component/map/Modal.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import ReactDOM from "react-dom";
+
+function Modal() {
+
+    const [displayModal, setDisplayModal] = useState(false);
+    
+}
+
+export default Modal;
