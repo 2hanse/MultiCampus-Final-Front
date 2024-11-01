a6e4569ff280c5bbb22398ba73e6164b9c222a40
rouie <streetstar03@gmail.com>
2024-11-01 14:43:11 +0900

    작업

--- src/component/map/bottomsheet/BottomSheet.jsx
+++ src/component/map/bottomsheet/BottomSheet.jsx
@@ -68,7 +68,7 @@ const Overlay = styled.div`
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: none;
     display: flex;
     justify-content: center;
     align-items: flex-end;
