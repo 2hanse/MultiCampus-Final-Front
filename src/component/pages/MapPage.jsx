9140ed2e6f65ed865f029d3519b928088da9a017
rouie <streetstar03@gmail.com>
2024-11-01 17:49:15 +0900

    Bottom Sheet

--- src/component/pages/MapPage.jsx
+++ src/component/pages/MapPage.jsx
@@ -31,16 +31,26 @@ function MapPage() {
 }
 
 const CustomSheet = styled(Sheet)`
+    display: flex;
+    position: absolute;
+    flex-direction: column;
+    align-items: flex-start;
     margin: 0 auto;
+    margin-bottom: 102px;
     max-width: 430px;
+    z-index: 5;
 
     /* sheet 라이브러리 css 덮어 쓰려면 !important 끝에 들어가야합니다 */
     .react-modal-sheet-backdrop {
-        background-color: rgba(0, 0, 0, 0.3) !important;
+        position: absolute !important;
+        width: 430px !important;
+        margin-bottom: 100px !important;
+        background-color: rgba(0, 0, 0, 0.1) !important;
     }
     .react-modal-sheet-container {
         background-color: #FFFFFF !important;
         border-radius: 20px 20px 0px 0px !important;
+        padding-top: 10px !important;
     }
     .react-modal-sheet-header {
         /* custom styles */
@@ -51,7 +61,7 @@ const CustomSheet = styled(Sheet)`
         cursor: grab;
     }
     .react-modal-sheet-content {
-        /* custom styles */
+        margin: 10px 20px !important;
     }
 `;
 
