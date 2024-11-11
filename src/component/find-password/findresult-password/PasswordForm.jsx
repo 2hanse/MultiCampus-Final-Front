1871ce3d99b4107102f1d476868a2cfc25d8810b
ehy0414 <djagusdyd78@gmail.com>
2024-11-11 09:42:03 +0900

    이메일확인 기능 수정

--- src/component/find-password/findresult-password/PasswordForm.jsx
+++ src/component/find-password/findresult-password/PasswordForm.jsx
@@ -73,12 +73,11 @@ function PasswordForm() {
         try {
             const response = await api.put("/users/reset-password", newPwd);
             if (response.status === 200) {
-                //console.log('유저 이메일: ' + response.data.email);
-                //console.log('새 비밀번호: ' + response.data.new_password); 
                 navigate('/user/resetPasswordResult');
             }
         } catch (err) {
             console.log('비밀번호 에러: ', err);
+            alert(err);
         }
         }
     };
